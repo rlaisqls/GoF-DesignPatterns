7d92bf7d6226f2e25234bd71086858f9aa8159c4
rlaisqls <rlaisqls@gmail.com>
2022-08-16 21:04:11 +0900

    📑 :: Prototype Pattrern

---
+++ src/main/java/com/study/gof/designpattrens/_01_CredentialPatterns/prototype/Pizza.java
@@ -0,0 +1,22 @@
+package com.study.gof.designpattrens._01_CredentialPatterns.prototype;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Pizza implements Cloneable{
+
+    String name;
+    String topping;
+    String sauce;
+
+    @Override
+    public Pizza clone() {
+        try {
+            return (Pizza) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+}
