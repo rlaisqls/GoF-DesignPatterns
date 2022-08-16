48ceaaed442e70e1253da062dcea989313b19211
rlaisqls <rlaisqls@gmail.com>
2022-08-16 21:05:14 +0900

    📑 :: Composite Pattern

---
+++ src/main/java/com/study/gof/designpattrens/_02_StructuralPatterns/composite/Composite.java
@@ -0,0 +1,7 @@
+package com.study.gof.designpattrens._02_StructuralPatterns.composite;
+
+
+public interface Composite {
+    String getName();
+    String getTree(String tab);
+}
---
+++ src/main/java/com/study/gof/designpattrens/_02_StructuralPatterns/composite/File.java
@@ -0,0 +1,22 @@
+package com.study.gof.designpattrens._02_StructuralPatterns.composite;
+
+public class File implements Composite{
+
+    private String name;
+
+    public File(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getTree(String tab) {
+        return "\n"
+                + tab + "File : " + this.getName() + "\n"
+                + tab.substring(2);
+    }
+}
