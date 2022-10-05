20834e11f300cda453d42fa1975daba5b419f61d
김은빈 <rlaisqls@gmail.com>
2022-10-05 12:47:38 +0900

    Update App.java

--- src/main/java/com/study/gof/designpattrens/_01_CredentialPatterns/builder/App.java
+++ src/main/java/com/study/gof/designpattrens/_01_CredentialPatterns/builder/App.java
@@ -4,7 +4,7 @@ public class App {
 
     public static void main(String[] args) {
 
-        //변수를 넣는 순서가 바뀌거나, 추가, 제거되도 유연하게 대처할 수 있다.
+        //변수를 넣는 순서가 바뀌거나, 추가, 제거돼도 유연하게 대처할 수 있다.
         Pizza pizza = Pizza.builder()
                 .sauce("Cheese sauce")
                 .name("Cheese pizza")
